31a750b71c34a5c688c2cbd38afe91f80a312acb
Thaleszh <thaleszh@gmail.com>
2017-06-02 18:59:52 -0300

    Create Finite_Automata.py

---
+++ Finite_Automata.py
@@ -0,0 +1,12 @@
+import state
+
+class Finite_Automata(object):
+	def __init__(self, states):
+		#	[transition, final, initial]
+		#	Example state: 
+		#	q0 = [[a : q2, b : q3], false, true]
+		#
+		self.fa_data = {}
+		for state in states:
+			self.fa_data[state] = [state.getTransitions(), state.getFinal(), state.getInitial()]
+			
