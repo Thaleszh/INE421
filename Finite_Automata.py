987b73e16a246f53972aa957430dbcbda15e1742
Thaleszh <thaleszh@gmail.com>
2017-06-02 19:03:25 -0300

    Update Finite_Automata.py

--- Finite_Automata.py
+++ Finite_Automata.py
@@ -5,7 +5,7 @@ class Finite_Automata(object):
     #	[transition, final, initial]
     #	Example state: 
     #	q0 = [[a : q2, b : q3], false, true]
-    #
+    
     self.fa_data = {}
     for state in states:
       self.fa_data[state] = [state[0], state[1], state.getInitial[2]]
