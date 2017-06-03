0cb02fff34184170cbcf9b4d23949d38d141fe1c
OttoMP <otto28mai@gmail.com>
2017-06-03 15:56:10 -0300

    bug fixes and minor improvements

--- Finite_Automata.py
+++ Finite_Automata.py
@@ -1,14 +1,11 @@
 
 
 class Finite_Automata(object):
-    initials = []
-    finals = []
-    transitions = {}
     
-    def __init__(self):
-        pass
-
     def __init__(self, states):
+        self.initials = []
+        self.finals = []
+        self.transitions = {}
         #	[transitions, final, initial]
         #	Example state: 
         #	q0 = [[a : q2, b : q3], false, true]
@@ -19,20 +16,47 @@ class Finite_Automata(object):
             if state.final is True:
                 self.finals.append(state.name)
             self.transitions[state.name] = state.transitions
-        
-    def create_state(self, state):
-        if state[2] is True:
+       
+    def make_final(state):
+        if state.final
+            self.finals.append(state.name)
+
+    def make_initial(state):
+        if state.initial
             self.initials.append(state.name)
-        if state[3] is True:
+
+    def create_state1(self, state):
+        if state.initial is True:
+            self.initials.append(state.name)
+        if state.final is True:
             self.finals.append(state.name)
         self.transitions[state.name] = state.transitions
 
-    def create_state(self, name, transitions, initial, final):
+    def create_state2(self, name, transitions, initial, final):
         if initial is True:
             self.initials.append(name)
         if final is True:
             self.finals.append(name)
         self.transitions[name] = transitions
+
+    def delete_state(self, state):
+        states = list(self.transitions.keys())
+        if state.name not in states:
+            print("state not found")
+            return
+        del self.transitions[state.name]
+        states = list(self.transitions.keys())
+        for queue in states:
+            keys = list(self.transitions[queue].keys())
+            for key in keys:
+                if state.name not in keys:
+                    pass
+                if self.transitions[queue][key] is state.name:
+                    del self.transitions[queue][key]
+        if state.name in self.initials:
+            self.initials.remove(state.name)
+        if state.name in self.finals:
+            self.finals.remove(state.name)
 
     def create_transition(self, state1, state2, key):
         states = list(self.transitions.keys())
--- main.py
+++ main.py
@@ -9,17 +9,30 @@ def main():
     estado0 = State(nome, transitions, inicial, final)
     estado1 = State("q1", {"a" : "q0", "b" : "q0"}, False, True)
     estado2 = State("q2", {"a" : "q1", "b" : "q0"}, False, False)
-    
-    fa_estados = [estado0]
+   
+    print(estado0.transitions)
+    print(estado1.transitions)
+    print(estado2.transitions)
 
+    fa_estados = [estado0]
     fa1 = Finite_Automata(fa_estados)
 
+    fa_estados2 = [estado2]
+    fa2 = Finite_Automata(fa_estados2)
+    
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+'''
     print(1)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
 
-    fa1.create_state("q1", {"a" : "q0"}, False, True)
+    fa1.create_state2("q1", {"a" : "q0"}, False, True)
     
     print("\n2")
     print(fa1.transitions)
@@ -45,4 +58,36 @@ def main():
     fa1.delete_transition(estado0, estado1, "c")
     fa1.delete_transition(estado0, estado1, "a")
 
+    fa1.delete_state(estado0)
+
+    print("\n6")
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+
+    fa1.create_state1(estado2)
+    fa1.create_state1(estado0)
+
+    print("\n7")
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+
+    print("\n8")
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+
+    fa2.create_transition(estado0, estado2, "b")
+
+    print("\n9")
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+
+    print("\n10")
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+'''
 main()
