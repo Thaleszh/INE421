f69d6927290d38e6002f30a52892fe529524ceab
OttoMP <otto28mai@gmail.com>
2017-06-04 12:46:39 -0300

    git earlie git often

--- FA_Algorithms.py
+++ FA_Algorithms.py
@@ -1,37 +1,45 @@
 import Finite_Automata
 
 def minimize(fa):
-    all_states = set(x for x in fa.transitions.keys())
-    
     #verificar estados acessiveis
     acessiveis = set(x for x in fa.initials)
-   
-
+    new_acessiveis = set()
     while acessiveis != new_acessiveis:
         new_acessiveis = acessiveis
         for s in acessiveis:
             for a in fa.transitions[s].keys():
                 new_acessiveis.add(fa.transitions[s][a])
 
-    for state in all_states not in acessiveis:
-        fa.delete_state(state)
+    for st in fa.states not in acessiveis:
+        fa.delete_state(st)
 
     #verificar estados mortos
     vivos = set(x for x in fa.finals)
-
+    new_vivos = set()
     while vivos != new_vivos:
         new_vivos = vivos
         for state in vivos
-            for s in all_states
+            for s in fa.states 
                 for a in fa.transitions[s].keys():
-                    if fa.transitions[s][a] is state.name
+                    if fa.transitions[s][a] is state
                         new_vivos.add(s)
 
-    for state in all_states not in vivos:
-        fa.delete_state(state)
+    for st in fa.states not in vivos:
+        fa.delete_state(st)
 
     #achar estados equivalentes
+    for a in self.alphabet:
+        fa.create_transition2("Fi", "Fi", a)
     
+    for s in self.states:
+        for k in self.alphabet not in self.transitions[s].keys():
+            fa.create_transition(s, "Fi", k)
+
+    set_F = set(x for x in fa.finals)
+    set_KF = set(x for x in fa.states not in finals}
+
+    for s in set_F:
+
 
 def determinize(fa):
     pass
