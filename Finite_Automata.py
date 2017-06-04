55a816ea7bff1336b60ab5a7c2a2c0c1b4b5f9d7
OttoMP <otto28mai@gmail.com>
2017-06-04 09:56:38 -0300

    States do not carry transitions anymore

--- Finite_Automata.py
+++ Finite_Automata.py
@@ -2,58 +2,57 @@
 
 class Finite_Automata(object):
     
-    def __init__(self, states):
+    def __init__(self):
         self.initials = []
         self.finals = []
         self.transitions = {}
-        self.alphabet = []
+        self.alphabet = [] 
+        self.states = []
         #	[transitions, final, initial]
         #	Example state: 
         #	q0 = [[a : q2, b : q3], false, true]
-    
-        for state in states:
-            if state.initial is True:
-                self.initials.append(state.name)
-            if state.final is True:
-                self.finals.append(state.name)
-            self.transitions[state.name] = state.transitions
-            for k in state.transitions.keys():
-                if k not in alphabet:
-                    self.alphabet.append(k)
-       
-    def add_final(state):
+   
+    def calculate_alphabet(self):
+        new_alphabet = []
+        for q in self.transitions.keys():
+            for k in self.transitions[q].keys():
+                if k not in new_alphabet:
+                    new_alphabet.append(k)
+        self.alphabet = new_alphabet
+
+    def add_final1(state):
         if state.final:
             self.finals.append(state.name)
 
-    def add_initial(state):
+    def add_initial1(state):
         if state.initial:
             self.initials.append(state.name)
 
+    def add_final2(name_state):
+        self.finals.append(name_state)
+
+    def add_initial2(name_state):
+        self.initials.append(name_state)
+
     def create_state1(self, state):
-        if state.initial is True:
+        if state.initial:
             self.initials.append(state.name)
-        if state.final is True:
+        if state.final:
             self.finals.append(state.name)
-        self.transitions[state.name] = state.transitions
-        for k in state.transitions.keys():
-            if k not in alphabet:
-                self.alphabet.append(k)
+        self.states.append(state.name)
 
-    def create_state2(self, name, transitions, initial, final):
-        if initial is True:
+    def create_state2(self, name, initial, final):
+        if initial:
             self.initials.append(name)
-        if final is True:
+        if final:
             self.finals.append(name)
-        self.transitions[name] = transitions
-        for k in state.transitions.keys():
-            if k not in alphabet:
-                self.alphabet.append(k)
+        self.states.append(name)
 
-    def delete_state(self, state):
-        states = list(self.transitions.keys())
-        if state.name not in states:
+    def delete_state1(self, state):
+        if state.name not in self.states:
             print("state not found")
             return
+        self.states.remove(state.name)
         del self.transitions[state.name]
         states = list(self.transitions.keys())
         for q in states:
@@ -68,19 +67,49 @@ class Finite_Automata(object):
         if state.name in self.finals:
             self.finals.remove(state.name)
 
-    def create_transition(self, state1, state2, key):
+    def delete_state2(self, name):
+        if name not in self.states:
+            print("state not found")
+            return
+        self.states.remove(name)
+        del self.transitions[name]
         states = list(self.transitions.keys())
-        if state1.name not in states:
-            self.create_state(state1.name, state1.transitions, state1.initial,
-                    state1.final)
-        if state2.name not in states:
-            self.create_state(state2.name, state2.transitions, state2.initial,
-                    state2.final)
-        self.transitions[state1.name][key] = state2.name
+        for q in states:
+            keys = list(self.transitions[q].keys())
+            for key in keys:
+                if name not in keys:
+                    pass
+                if self.transitions[q][key] is name:
+                    del self.transitions[q][key]
+        if name in self.initials:
+            self.initials.remove(name)
+        if name in self.finals:
+            self.finals.remove(name)
 
-    def delete_transition(self, state1, state2, key):
-        states = list(self.transitions.keys())
-        if state1.name not in states:
+    def create_transition_aux(self, name):
+        if name not in self.transitions.keys():
+            self.transitions[name] = {}
+
+    def create_transition1(self, state1, state2, key):
+        if state1.name not in self.states:
+            self.create_state1(state1)
+        if state2.name not in self.states:
+            self.create_state1(state2)
+        self.create_transition_aux(state1.name)
+        self.transitions[state1.name][key] = state2.name
+        self.calculate_alphabet()
+
+    def create_transition2(self, name_state1, name_state2, key):
+        if name_state1 not in self.states:
+            self.create_state2(name_state1, False, False)
+        if name_state2 not in self.states:
+            self.create_state2(name_state2, False, False)
+        self.create_transition_aux(name_state1)
+        self.transitions[name_state1][key] = name_state2
+        self.calculate_alphabet()
+
+    def delete_transition1(self, state1, state2, key):
+        if state1.name not in self.states:
             print("state not found")
             return
         state1_keys = list(self.transitions[state1.name])
@@ -91,12 +120,26 @@ class Finite_Automata(object):
             print("transition not found")
             return
         del self.transitions[state1.name][key]
+        self.calculate_alphabet()
+        
+    def delete_transition2(self, name_state1, name_state2, key):
+        if name_state1 not in self.states:
+            print("state not found")
+            return
+        state1_keys = list(self.transitions[name_state1])
+        if key not in state1_keys:
+            print("key not found")
+            return
+        if name_state2 is not self.transitions[name_state1][key]:
+            print("transition not found")
+            return
+        del self.transitions[name_state1][key]
+        self.calculate_alphabet()
         
 
 class State:
 
-    def __init__(self, name, transitions, initial, final):
+    def __init__(self, name, initial, final):
         self.name = name
-        self.transitions = transitions
         self.initial = initial
         self.final = final
--- main.py
+++ main.py
@@ -1,93 +1,154 @@
 from Finite_Automata import Finite_Automata, State
 
 def main():
-    transitions = {"a": "q0"}
     final = True
     inicial = True
     nome = "q0"
 
-    estado0 = State(nome, transitions, inicial, final)
-    estado1 = State("q1", {"a" : "q0", "b" : "q0"}, False, True)
-    estado2 = State("q2", {"a" : "q1", "b" : "q0"}, False, False)
-   
-    print(estado0.transitions)
-    print(estado1.transitions)
-    print(estado2.transitions)
+    estado0 = State(nome, inicial, final)
+    estado1 = State("q1", False, True)
+    estado2 = State("q2", False, False)
+    estado3 = State("q3", False, False)
 
-    fa_estados = [estado0]
-    fa1 = Finite_Automata(fa_estados)
+    fa1 = Finite_Automata()
 
-    fa_estados2 = [estado2]
-    fa2 = Finite_Automata(fa_estados2)
+    fa2 = Finite_Automata()
     
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
     print(fa2.transitions)
     print(fa2.initials)
     print(fa2.finals)
-'''
-    print(1)
+
+    print("\n1")
+
+    fa1.create_state1(estado0)
+    fa1.create_state1(estado1)
+    fa1.create_state1(estado2)
+
+    fa2.create_state2("S", True, False)
+    fa2.create_state2("A", False, False)
+    fa2.create_state2("B", False, True)
+
+
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
 
-    fa1.create_state2("q1", {"a" : "q0"}, False, True)
-    
+
     print("\n2")
-    print(fa1.transitions)
-    print(fa1.initials)
-    print(fa1.finals)
 
-    fa1.create_transition(estado0, estado1, "b")
+    fa1.create_transition1(estado0, estado1, "a")
+    fa1.create_transition1(estado1, estado2, "b")
+    fa1.create_transition1(estado2, estado0, "c")
+    fa2.create_transition2("S", "A", "a")
+    fa2.create_transition2("A", "B", "b")
+    fa2.create_transition2("B", "S", "c")
     
-    print("\n3")
+    
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
 
-    fa1.delete_transition(estado0, estado1, "b")
+   
+    print("\n3")
+
+    fa1.create_transition1(estado0, estado3, "b")
+    fa2.create_transition2("S", "D", "d")
+
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+
 
     print("\n4")
+
+    fa1.delete_transition1(estado0, estado3, "b")
+    fa2.delete_transition2("S", "D", "d")
+
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+
 
     print("\n5")
-    fa1.delete_transition(estado2, estado1, "c")
-    fa1.delete_transition(estado0, estado1, "c")
-    fa1.delete_transition(estado0, estado1, "a")
 
-    fa1.delete_state(estado0)
+    fa1.delete_state1(estado2)
+    fa2.delete_state2("A")
+
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
+    print(fa1.transitions)
+    print(fa1.initials)
+    print(fa1.finals)
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
+    print(fa2.transitions)
+    print(fa2.initials)
+    print(fa2.finals)
+
 
     print("\n6")
+
+    fa1.delete_state1(estado2)
+    fa2.delete_state2("C")
+
+    print("fa1")
+    print(fa1.states)
+    print(fa1.alphabet)
     print(fa1.transitions)
     print(fa1.initials)
     print(fa1.finals)
-
-    fa1.create_state1(estado2)
-    fa1.create_state1(estado0)
-
-    print("\n7")
-    print(fa1.transitions)
-    print(fa1.initials)
-    print(fa1.finals)
-
-    print("\n8")
+    print("fa2")
+    print(fa2.states)
+    print(fa2.alphabet)
     print(fa2.transitions)
     print(fa2.initials)
     print(fa2.finals)
 
-    fa2.create_transition(estado0, estado2, "b")
-
-    print("\n9")
-    print(fa2.transitions)
-    print(fa2.initials)
-    print(fa2.finals)
-
-    print("\n10")
-    print(fa1.transitions)
-    print(fa1.initials)
-    print(fa1.finals)
-'''
 main()
