46cca137956607e481b8663f8daefd24e276cc3f
Thaleszh <thaleszh@gmail.com>
2017-06-19 22:02:00 -0300

    added regular expressions

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import *
 from Finite_Automata import Finite_Automata
 from FA_Algorithms import *
+import RE_Algorithms
 
 class gui():
 
@@ -11,6 +12,7 @@ class gui():
 		self.FA_list = []
 		self.RE_list = []
 		self.current_fa = None
+		self.current_re = ''
 		self.initUI()
 
 	def initUI(self):
@@ -26,6 +28,8 @@ class gui():
 		self.grid.setColumnStretch(1, 4)
 		self.grid.setColumnStretch(2, 4)
 		self.grid.setSpacing(5)
+		# status bar
+		self.main_window.statusBar()
 
         # -- menu bar --
 		self.menubar = self.main_window.menuBar()
@@ -55,12 +59,6 @@ class gui():
 		# edit menu bar actions
 		self.edit_menu = self.menubar.addMenu('&Edit')
 
-		self.close = QAction(QIcon('state.png'), '&Close Tab', self.main_window)
-		#self.close.setShortcut('Ctrl+S')
-		self.close.setStatusTip('Closes current tb')
-		self.close.triggered.connect(self.close_event)
-		self.edit_menu.addAction(self.close)
-
 		# FA options:
 		self.new_state = QAction(QIcon('state.png'), '&Create State', self.main_window)
 		self.new_state.setShortcut('Ctrl+S')
@@ -74,6 +72,12 @@ class gui():
 		self.delete_state.triggered.connect(self.delete_state_event)
 		self.edit_menu.addAction(self.delete_state)
 
+		self.change_alphabet = QAction(QIcon('state.png'), '&Edit Alphabet', self.main_window)
+		self.change_alphabet.setShortcut('Ctrl+A')
+		self.change_alphabet.setStatusTip('Changes the alphabet of current automata')
+		self.change_alphabet.triggered.connect(self.change_alphabet_event)
+		self.edit_menu.addAction(self.change_alphabet)
+
 		self.add_final_state = QAction(QIcon('state.png'), '&Add state to finals', self.main_window)
 		self.add_final_state.setShortcut('Ctrl+F')
 		self.add_final_state.setStatusTip('Add a state to final set in current FA')
@@ -85,6 +89,18 @@ class gui():
 		self.set_initial_state.setStatusTip('Set a state as initial of this AF')
 		self.set_initial_state.triggered.connect(self.set_initial_state_event)
 		self.edit_menu.addAction(self.set_initial_state)
+
+		self.determinize = QAction(QIcon('state.png'), '&Determinize', self.main_window)
+		self.determinize.setShortcut('Ctrl+Shift+D')
+		self.determinize.setStatusTip('Determinizes the current FA')
+		self.determinize.triggered.connect(self.determinize_event)
+		self.edit_menu.addAction(self.determinize)
+
+		self.minimize = QAction(QIcon('state.png'), '&Minimize', self.main_window)
+		self.minimize.setShortcut('Ctrl+Shift+M')
+		self.minimize.setStatusTip('Minimizes the current FA')
+		self.minimize.triggered.connect(self.minimize_event)
+		self.edit_menu.addAction(self.minimize)
 
 		self.unite = QAction(QIcon('state.png'), '&Union', self.main_window)
 		self.unite.setShortcut('Ctrl+U')
@@ -110,18 +126,39 @@ class gui():
 		self.diff.triggered.connect(self.diff_event)
 		self.edit_menu.addAction(self.diff)
 
-		self.minimize = QAction(QIcon('state.png'), '&Minimize', self.main_window)
-		self.minimize.setShortcut('Ctrl+Shift+M')
-		self.minimize.setStatusTip('Minimizes the current FA')
-		self.minimize.triggered.connect(self.minimize_event)
-		self.edit_menu.addAction(self.minimize)
+		self.edit_menu.addSeparator()
+		# RE Options
 
-		self.determinize = QAction(QIcon('state.png'), '&Determinize', self.main_window)
-		self.determinize.setShortcut('Ctrl+Shift+D')
-		self.determinize.setStatusTip('Determinizes the current FA')
-		self.determinize.triggered.connect(self.determinize_event)
-		self.edit_menu.addAction(self.determinize)
+		self.generate_automata = QAction(QIcon('state.png'), '&Generate Automata', self.main_window)
+		self.generate_automata.setShortcut('Ctrl+G')
+		self.generate_automata.setStatusTip('Creates the automata from this Regular Expression')
+		self.generate_automata.triggered.connect(self.generate_automata_event)
+		self.edit_menu.addAction(self.generate_automata)
+
+		self.edit_expression = QAction(QIcon('state.png'), '&Edit Expression', self.main_window)
+		self.edit_expression.setShortcut('Ctrl+E')
+		self.edit_expression.setStatusTip('Edits the expression')
+		self.edit_expression.triggered.connect(self.edit_expression_event)
+		self.edit_menu.addAction(self.edit_expression)
+
+		self.compare_expression = QAction(QIcon('state.png'), '&Compare Expression', self.main_window)
+		self.compare_expression.setShortcut('Ctrl+Alt+C')
+		self.compare_expression.setStatusTip('Determines if the expression is equal to target expression')
+		self.compare_expression.triggered.connect(self.compare_expression_event)
+		self.edit_menu.addAction(self.compare_expression)
+
+
+		self.edit_menu.addSeparator()
+
+
+		self.close = QAction(QIcon('state.png'), '&Close Tab', self.main_window)
+		#self.close.setShortcut('Ctrl+S')
+		self.close.setStatusTip('Closes current tb')
+		self.close.triggered.connect(self.close_event)
+		self.edit_menu.addAction(self.close)
+
 		self.allow_FA(0)
+		self.allow_RE(0)
 
 		# tab
 
@@ -138,12 +175,13 @@ class gui():
 		self.main_window.setGeometry(300,300,800,600)
 		self.main_window.show()
 
-		self.create_fa('m1', 'ab')
-		self.create_state('q0', True, True)
-		self.create_fa('m2', 'abc')
+		#self.create_fa('m1', 'ab')
+		#self.create_state('q0', True, True)
+		#self.create_fa('m2', 'abc')
 
 	def allow_FA(self, boolea):
 		if boolea:
+			self.allow_RE(False)
 			self.determinize.setEnabled(True)
 			self.minimize.setEnabled(True)
 			self.diff.setEnabled(True)
@@ -155,6 +193,7 @@ class gui():
 			self.close.setEnabled(True)
 			self.add_final_state.setEnabled(True)
 			self.set_initial_state.setEnabled(True)
+			self.change_alphabet.setEnabled(True)
 		else:
 			self.determinize.setEnabled(False)
 			self.minimize.setEnabled(False)
@@ -167,6 +206,75 @@ class gui():
 			self.close.setEnabled(False)
 			self.add_final_state.setEnabled(False)
 			self.set_initial_state.setEnabled(False)
+			self.change_alphabet.setEnabled(False)
+
+	def allow_RE(self, boolea):
+		if boolea:
+			self.allow_FA(False)
+			self.generate_automata.setEnabled(True)
+			self.edit_expression.setEnabled(True)
+			self.compare_expression.setEnabled(True)
+			self.close.setEnabled(True)
+
+		else:
+			self.generate_automata.setEnabled(False)
+			self.edit_expression.setEnabled(False)
+			self.compare_expression.setEnabled(False)
+			self.close.setEnabled(False)
+
+	def generate_automata_event(self):
+		state_name, ok = QInputDialog.getText(self.main_window, 'Generate Automata', 'Name the created automata:')
+		if ok:
+			self.generate_automata_(state_name)
+
+	def generate_automata_(self, automata_name):
+		current_tab = self.tabs.currentWidget()
+		name = current_tab.name
+		print('Tab name: ' + name)
+		print('Current Regular Expression: ' + self.current_re)
+		for regular_expression in self.RE_list:
+			if regular_expression[0] == name:
+				self.current_re = regular_expression[1]
+				print('Regular Expression changed: ' + self.current_re)
+		new_fa = RE_Algorithms.de_simone(self.current_re)
+		new_fa.name = automata_name
+		self.FA_list.append(new_fa)
+		self.add_tab(new_fa.name, ['FA', new_fa.alphabet])
+
+	def edit_expression_event(self):
+		new_expression, ok = QInputDialog.getText(self.main_window, 'Edit Expression', 'New Expression:')
+		if ok:
+			current_tab = self.tabs.currentWidget()
+			name = current_tab.name
+			for regular_expression in self.RE_list:
+				if regular_expression[0] == name:
+					regular_expression[1] = new_expression
+					current_re = new_expression
+					print('Expression: ' + current_re)
+					current_tab.label.setText('Regular Expression: ' + current_re)
+					break
+
+	def compare_expression_event(self):
+		found = False
+		while not found:
+			expression, ok = QInputDialog.getText(self.main_window, 'Compare', 'What is the name of the second expression?')
+			if ok:
+				second_expression = ''
+				for regular_expression in self.RE_list:
+					if regular_expression[0] == expression:
+						found = True
+						second_expression = regular_expression[1]
+						break
+				if found:
+					result = equivalence(RE_Algorithms.de_simone(self.current_re), RE_Algorithms.de_simone(second_expression))
+					if result:
+						end = QMessageBox.question(self.main_window, 'Equal', 'The second expression is equal the first one', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)					
+					else:
+						end = QMessageBox.question(self.main_window, 'Not Equal', 'The second expression is not equal the first one', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)											
+				else:
+					again = QMessageBox.question(self.main_window, 'Not Found', 'The second expression was not found, try again?', QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+					if again == QMessageBox.No:
+						break
 
 	def create_state_event(self):
 		state_name, ok = QInputDialog.getText(self.main_window, 'Create State', 'Name the state:')
@@ -202,6 +310,16 @@ class gui():
 	def remove_state(self, name):
 		self.current_fa.delete_state(name)
 		self.update_table()
+
+	def change_alphabet_event(self):
+		alphabet, ok = QInputDialog.getText(self.main_window, 'New Alphabet', 'New alphabet: ')
+		if ok:
+			current_tab = self.tabs.currentWidget()
+			current_tab.characters.clear()
+			for character in alphabet:
+				current_tab.characters.append(character)
+			current_tab.table_widget.setColumnCount(len(current_tab.characters))
+			current_tab.table_widget.setHorizontalHeaderLabels(current_tab.characters)
 
 	def add_final_state_event(self):
 		final_state, ok = QInputDialog.getText(self.main_window, 'Final State', 'Name of the state:')
@@ -425,7 +543,7 @@ class gui():
 	def new_fa(self):
 		self.expression, ok = QInputDialog.getText(self.main_window, 'FA Input', 'Enter the name of the Finite Automata: ')
 		if ok:
-			alphabet, ok = QInputDialog.getText(self.main_window, 'Alphabet Input', 'Enter the alphabet of ' + self.expression)
+			alphabet, ok = QInputDialog.getText(self.main_window, 'Alphabet Input', 'Enter the alphabet of ' + self.expression + '. (Example: abc)')
 			self.create_fa(self.expression, alphabet)
 
 	def create_fa(self, expression, alphabet):
@@ -435,18 +553,35 @@ class gui():
 		self.add_tab(expression, ['FA', alphabet])
 
 	def new_re(self):
-		self.expression, ok = QInputDialog.getText(self.main_window, 'RE Input', 'Enter the name of the Regular Expression: ')
+		name, ok = QInputDialog.getText(self.main_window, 'RE Input', 'Enter the name of the Regular Expression: ')
 		if ok:
-			re = QLabel('Regular Expression: ' + str(self.expression))
+			self.expression, ok = QInputDialog.getText(self.main_window, 'RE Input', 'Enter the regular Expression: (Example: (a?|ba)* )')
+			regular_expression = str(self.expression)
+			self.create_re_(name, regular_expression)
+
+	def create_re_(self, name, expression):
+		self.RE_list.append([name, expression])
+		self.current_re = expression
+		self.add_tab(name, ['RE', expression])
 
 	def cell_changed_event(self, row, column):
 		if not self.disable_changes:
 			current_tab = self.tabs.currentWidget()
 			current_item = current_tab.table_widget.item(row, column)
 			if current_item:
-				if current_item.text() != '' :
+				text = current_item.text()
+				# replace not working
+				temp_tex = text.split(" ")
+				text = ''
+				for string in temp_tex:
+					text += string
+				print(text)
+				if text != '' :
 					self.current_fa.delete_all_transitions(current_tab.states[row], current_tab.characters[column])
-					self.current_fa.create_transition(current_tab.states[row], current_item.text() , current_tab.characters[column])
+					texts = text.split(',')
+					print(texts)
+					for new_text in texts:
+						self.current_fa.create_transition(current_tab.states[row], new_text , current_tab.characters[column])
 				else:
 					self.current_fa.delete_all_transitions(current_tab.states[row], current_tab.characters[column])
 				#might have added new state
@@ -480,9 +615,7 @@ class gui():
 			if state in self.current_fa.transitions:
 				keys = self.current_fa.transitions[state].keys()
 				for key in keys:
-					item = current_tab.table_widget.item(index, current_tab.characters.index(key))
-					if item:
-						item.setText(self.current_fa.transitions[state][key])
+					current_tab.table_widget.setItem(index, current_tab.characters.index(key), QTableWidgetItem(self.current_fa.transitions[state][key]))
 		self.disable_changes = False
 
 	def add_tab(self, tab_name, tab_type):
@@ -494,30 +627,40 @@ class gui():
 	def close_event(self):
 		current_tab = self.tabs.currentWidget()
 		self.tabs.removeTab(self.tabs.indexOf(current_tab))
+		self.FA_list.remove(self.current_fa)
 
 	# tab changed event
 	def tab_changed(self):
 		current_tab = self.tabs.currentWidget()
-		if current_tab is not None and current_tab.type == 'FA':
-			self.allow_FA(True)
-			for fa in self.FA_list:
-					if fa.name == current_tab.name:
-						self.current_fa = fa
+		if current_tab is not None:
+			# FA
+			if current_tab.type == 'FA':
+				self.allow_FA(True)
+				for fa in self.FA_list:
+						if fa.name == current_tab.name:
+							self.current_fa = fa
+							break
+				print()
+				print("Current FA: " + self.current_fa.name)
+				print('initial state: ' + self.current_fa.initials)
+				print('States:')
+				print(self.current_fa.states)
+				print('Finals:')
+				print(self.current_fa.finals)
+				print('Transitions: ')
+				print(self.current_fa.transitions)
+				print('Alphabet: ')
+				print(self.current_fa.alphabet)		
+				self.update_table()
+
+			# RE
+			elif current_tab.type == 'RE':
+				self.allow_RE(True)
+				for regular_expression in self.RE_list:
+					if regular_expression[0] == current_tab.name:
+						self.current_re = regular_expression[1]
 						break
-			print()
-			print("Current FA: " + self.current_fa.name)
-			print('initial state: ' + self.current_fa.initials)
-			print('States:')
-			print(self.current_fa.states)
-			print('Finals:')
-			print(self.current_fa.finals)
-			print('Transitions: ')
-			print(self.current_fa.transitions)
-			print('Alphabet: ')
-			print(self.current_fa.alphabet)		
-			self.update_table()
-		else:
-			self.allow_FA(False)
+
 
 class Tab(QWidget):
 	def __init__(self, tab_name, tab_type):
@@ -529,18 +672,22 @@ class Tab(QWidget):
 		self.states = list()
 		self.states_extra = list()
 		self.layout = QVBoxLayout(self)
+		self.expression = ''
+		self.label = QLabel()
 		if self.type == 'FA':
-			self.characters = list()
 			for character in tab_type[1]:
 				self.characters.append(character)
 			self.table_widget.setColumnCount(len(self.characters))
 			self.table_widget.setHorizontalHeaderLabels(self.characters)
 			self.layout.addWidget(self.table_widget)
-			self.setLayout(self.layout)
+		elif self.type == 'RE':
+			self.expression = tab_type[1]
+			self.label = QLabel('Regular Expression: ' + self.expression, self)
+			self.layout.addWidget(self.label)
+		self.setLayout(self.layout)
 
 if __name__ == '__main__':
 
 	app = QApplication(sys.argv)
 	ex = gui()
 	sys.exit(app.exec_())  
-
